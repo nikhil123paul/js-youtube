f16eb5e3da3e25f43822e446efaffbd34eedfe7c
Nikhil <163821993+nikhil123paul@users.noreply.github.com>
2025-05-28 11:16:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+ const accountId = 9880;
+ let nk = "nikhil";
+ var male = "gender";
+ city = "Muzaffarnagar"
+
+
+ /* don't  use var because of issue in block scope and functional scope*/
+ 
+ nk= "nik"
+ male="female"
+ city="udaipur"
+
+ console.table([accountId , nk, male , city]);
+ 
