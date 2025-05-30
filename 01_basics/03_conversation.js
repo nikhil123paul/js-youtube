d6563fbff945974d619300dc470109f9d5b46119
Nikhil <163821993+nikhil123paul@users.noreply.github.com>
2025-05-30 05:38:43 +0000

    conversation

---
+++ 01_basics/03_conversation.js
@@ -0,0 +1,22 @@
+let score = null
+//console.log( typeof score);
+
+let valueInString = String(score)
+//console.log(valueInString);
+//console.log(typeof valueInString);
+
+
+//  when we convert string to number than
+// "33" => 33
+// "33abc"=> NaN
+// true => 1; false=> 0
+
+let isLoggedIn = "nikhil"
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=> true , 0 => false
+//  ""=> false
+//  "nikhil"=> true
+
+
+
