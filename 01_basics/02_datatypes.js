be4a4f7a0b96b47dffb3db9159bb35bf0ffc9526
Nikhil <163821993+nikhil123paul@users.noreply.github.com>
2025-05-30 05:38:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean=> true/false
+// null=> standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log( typeof undefined); // type of in undefined
+console.log(typeof null); //alwayes the type of is object
+
+
