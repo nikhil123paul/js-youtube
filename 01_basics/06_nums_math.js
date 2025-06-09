88329792302a92260b6206098b64dd3801826574
Nikhil <163821993+nikhil123paul@users.noreply.github.com>
2025-06-09 07:04:54 +0000

    math and num

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,29 @@
+const value = 400
+// console.log(typeof(value));
+
+const count = new Number(300)
+// console.log(count);
+// console.log(typeof(count));
+
+const numeric = 123.99
+// console.log(numeric.toPrecision(3));
+// console.log(numeric.toFixed(1));
+
+
+
+
+//    maths trick
+
+// console.log(Math.random() * 10 +1)
+
+// when we want , the num is between 10 to 20 
+ max= 20
+ min = 10
+
+ console.log( Math.floor(Math.random() * (max - min + 1) + min));
+ 
+
+
+
+
+
