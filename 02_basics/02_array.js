1a8b8f040b6f239e8874288a4cd15766c63f4c35
Nikhil <163821993+nikhil123paul@users.noreply.github.com>
2025-06-19 08:45:55 +0000

    array2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,30 @@
+ const myTeam = [ "mangu" , "nikhil" , "hardik" , "agrim"]
+ const popiTeam = [ "popi" , "kunal" , "kittu" , "keshav"]
+
+//  console.log(myTeam.concat(popiTeam));
+//   console.log([...myTeam , ...popiTeam]);
+
+const newArray = [1, 2, [3, 4] , 8, 9, [10, [ 11,12 ],],]
+// console.log(newArray.flat());
+
+
+// const New= "nikhil"
+const my = {name : "nikhl"}
+console.log(Array.from(my));
+
+let a = 200
+let b= 400
+let c= 300
+console.log(Array.of(a, b, c));
+
+
+
+
+
+
+ 
+ 
+ 
+
+
+ 
